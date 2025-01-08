275753cc223b6b91a733ac68aad5940340c02ce8
ranselorm <ranselorm@outlook.com>
2025-01-08 08:23:44 +0000

    Add contact hero

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,8 +1,14 @@
 import ContactDetails from "@/components/ContactDetails";
+import ProductHero from "@/components/ProductHero";
 
 const Contact = () => {
   return (
     <section>
+      <ProductHero
+        backgroundImage="/images/contact.jpg"
+        title="Contact"
+        subtitle="Get in Touch. We're Here to Help!"
+      />
       <ContactDetails />
     </section>
   );
