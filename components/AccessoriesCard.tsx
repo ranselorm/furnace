31b90ea6de4572120d1889088ae26f1ba679c22f
ranselorm <ranselorm@outlook.com>
2025-02-23 23:33:43 +0000

    Refactor

--- components/AccessoriesCard.tsx
+++ components/AccessoriesCard.tsx
@@ -23,7 +23,6 @@ const AccessoriesCard: React.FC<AccessoriesCardProps> = ({
 }) => {
   return (
     <div className="max-w-xs shadow-md flex flex-col justify-between self-center h-96">
-      {/* Image Section */}
       <div className="relative h-48 bg-gray-300 flex items-center justify-center hover:scale-105 transition-all duration-300">
         <img
           src={image}
@@ -32,7 +31,6 @@ const AccessoriesCard: React.FC<AccessoriesCardProps> = ({
         />
       </div>
 
-      {/* Content Section */}
       <div className="p-6 flex flex-col items-center justify-between flex-1 pt-20">
         <Dialog>
           <h3 className="text-base text-center">{title}</h3>
@@ -43,7 +41,6 @@ const AccessoriesCard: React.FC<AccessoriesCardProps> = ({
           </DialogTrigger>
           <DialogContent className="p-0 border-none max-w-[90%] md:max-w-[60%] h-[400px] rounded-lg">
             <div className="flex flex-col md:flex-row bg-white rounded-lg overflow-hidden">
-              {/* Image Section */}
               <div className="w-full md:w-1/2 h-64 md:h-full flex items-center justify-center bg-gray-100">
                 <img
                   src={image}
@@ -51,7 +48,6 @@ const AccessoriesCard: React.FC<AccessoriesCardProps> = ({
                   className="w-full h-full object-contain p-4"
                 />
               </div>
-              {/* Details Section */}
               <div className="p-6 flex flex-col justify-center w-full md:w-1/2">
                 <DialogHeader>
                   <DialogTitle className="text-xl font-bold">
