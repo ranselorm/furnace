bfd533bde8848cda93496b492e8f8c57633b1be6
ranselorm <ranselorm@outlook.com>
2025-02-23 23:26:49 +0000

    Refactor

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,48 +1,46 @@
 import React from "react";
 import ServiceCard from "./ServiceCard";
-import BuyCard from "./BuyCard";
-import Gallery from "./Gallery";
+
+const services = [
+  {
+    image: "/images/banner1.jpg",
+    icon: "game-icons:furnace",
+    title: "FURNACES",
+    description:
+      "Our industry-leading heating products are highly automated with controllable temperatures.",
+    linkText: "LEARN MORE",
+    linkHref: "/furnaces",
+  },
+  {
+    image: "/images/banner2.png",
+    icon: "mdi:water-boiler",
+    title: "BOILERS",
+    description:
+      "With tailored solutions for industry, our boilers offer precise temperature control and powerful performance.",
+    linkText: "COMMERCIAL HVAC SYSTEMS",
+    linkHref: "/commercial-hvac",
+  },
+  {
+    image: "/images/banner3.png",
+    title: "KILNS",
+    icon: "lucide:flame",
+    description:
+      "For over 25 years, we've proudly offered a diverse and varied range of heating solutions that are safe and reliable for Industry and Mining companies",
+    linkText: "RESIDENTIAL HEATING",
+    linkHref: "/residential-heating",
+  },
+  {
+    image: "/images/banner3.png",
+    title: "CALCINATION OVENS",
+    icon: "material-symbols:oven",
+    description:
+      "Having replaced bricks with cast grooved refractory slabs, our calcine ovens have become the most preferred calcination ovens for mining companies.",
+    linkText: "RESIDENTIAL HEATING",
+    linkHref: "/residential-heating",
+  },
+];
 
 const ServicesSection: React.FC = () => {
-  const services = [
-    {
-      image: "/images/banner1.jpg",
-      icon: "game-icons:furnace",
-      title: "FURNACES",
-      description:
-        "Our industry-leading heating products are highly automated with controllable temperatures.",
-      linkText: "LEARN MORE",
-      linkHref: "/furnaces",
-    },
-    {
-      image: "/images/banner2.png",
-      icon: "mdi:water-boiler",
-      title: "BOILERS",
-      description:
-        "With tailored solutions for industry, our boilers offer precise temperature control and powerful performance.",
-      linkText: "COMMERCIAL HVAC SYSTEMS",
-      linkHref: "/commercial-hvac",
-    },
-    {
-      image: "/images/banner3.png",
-      title: "KILNS",
-      icon: "lucide:flame",
-      description:
-        "For over 25 years, we've proudly offered a diverse and varied range of heating solutions that are safe and reliable for Industry and Mining companies",
-      linkText: "RESIDENTIAL HEATING",
-      linkHref: "/residential-heating",
-    },
-    {
-      image: "/images/banner3.png",
-      title: "CALCINATION OVENS",
-      icon: "material-symbols:oven",
-      description:
-        "Having replaced bricks with cast grooved refractory slabs, our calcine ovens have become the most preferred calcination ovens for mining companies.",
-      linkText: "RESIDENTIAL HEATING",
-      linkHref: "/residential-heating",
-    },
-  ];
-
   return (
     <section className="py-20 bg-gray-100">
       <div className="container mx-auto px-4 md:px-0 text-center">
