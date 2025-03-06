3597d90b80fd867601396783ee6e774e6a16d84c
ranselorm <ranselorm@outlook.com>
2025-03-06 17:57:51 +0000

    Modify for seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <title>Caesar Furnace</title>
-      <body className="antialiased">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="author" content="Caesar Furnace Team" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
