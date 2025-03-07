557c6a8dacd70ac8c1a2504ad21c8d852cfbbf26
ranselorm <ranselorm@outlook.com>
2025-03-07 21:25:01 +0000

    Cleanup

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,5 @@
-// src/components/Footer.tsx
 import React from "react";
 import Link from "next/link";
-// import Socials from "./Socials";
 
 const Footer: React.FC = () => {
   const importantLinks = [
@@ -14,16 +12,8 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-700 text-white py-12">
       <div className="container mx-auto px-4 md:px-8 lg:px-16 grid grid-cols-1 md:grid-cols-3 gap-8">
-        {/* Logo and Important Links */}
         <div>
-          <div className="mb-6">
-            {/* <img
-              src="/images/logo-white.png"
-              alt="BRL Mining Exploration"
-              className="h-12"
-            /> */}
-            Caesar Furnace
-          </div>
+          <div className="mb-6">Caesar Furnace</div>
           <ul className="space-y-2">
             {importantLinks.map((link, index) => (
               <li key={index}>
@@ -38,7 +28,6 @@ const Footer: React.FC = () => {
           </ul>
         </div>
 
-        {/* Location Information */}
         <div>
           <h3 className="text-lg font-semibold mb-4">Location</h3>
           <address className="not-italic text-gray-300">
@@ -71,7 +60,6 @@ const Footer: React.FC = () => {
           </address>
         </div>
 
-        {/* Newsletter Subscription */}
         <div>
           <h3 className="text-lg font-semibold mb-4">Newsletter</h3>
           <p className="text-gray-300 mb-4">
@@ -99,7 +87,6 @@ const Footer: React.FC = () => {
         </div>
       </div>
 
-      {/* Bottom Footer */}
       <div className="border-t border-gray-700 mt-8 pt-4">
         <div className="container mx-auto px-4 md:px-8 lg:px-16 flex flex-col md:flex-row justify-between items-center">
           <p className="text-gray-400 text-sm">
