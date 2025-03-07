4fafa770022fab0216967543f40bd9177b0becb2
ranselorm <ranselorm@outlook.com>
2025-03-07 21:21:13 +0000

    Minor update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.caesarfurnacegh.com", // Your website URL
-  generateRobotsTxt: true, // Generate a robots.txt file
-  sitemapSize: 5000, // Maximum number of URLs per sitemap file
-  exclude: ["/admin", "/dashboard"], // Exclude sensitive routes
-  changefreq: "daily", // Tell search engines how often pages update
-  priority: 0.7, // Default priority for pages
+  siteUrl: "https://www.caesarfurnacegh.com",
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  exclude: ["/admin", "/dashboard"],
+  changefreq: "daily",
+  priority: 0.7,
 };
