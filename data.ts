f996782cc99c112e048fd8f049cd7f6cc00f1073
ranselorm <ranselorm@outlook.com>
2025-05-30 16:07:20 +0000

    Install next seo

---
+++ data.ts
@@ -0,0 +1,59 @@
+export const products = [
+  {
+    image: "/images/furn.jpg",
+    title: "SMELTING FURNACES",
+
+    description:
+      "Built for Asanko Gold, FGR Bogosu, Mensin Gold Bibiani, Gold Recovery Tema, and Chirano Goldmines.",
+  },
+  {
+    image: "/images/calcine.jpg",
+    title: "CALCINE OVENS",
+
+    description:
+      "Built this Calcine Oven for Mensin Gold Bibiani, AngloGold Ashanti (Iduapriem Tarkwa), Asanko Gold, FGR Bogosu, former AGC Obuasi mines.",
+  },
+  {
+    image: "/images/boiler.jpg",
+    title: "BOILERS",
+
+    description:
+      "Reliable and energy-efficient boilers designed for producing high-pressure steam or hot water for industrial and commercial heating needs.",
+  },
+  {
+    image: "/images/incen.jpg",
+    title: "INCINERATORS",
+
+    description:
+      "Built this incinerator and wet scrubber at the Caesar Furnace & Kilns Commercial Incineration Center in Kumasi.",
+  },
+  {
+    image: "/images/dryer.jpg",
+    title: "DRYERS",
+
+    description:
+      "Built this incinerator for Movis Logistics for the incineration of cyanide packages.",
+  },
+  {
+    image: "/images/kiln.jpg",
+    title: "KILNS",
+
+    description:
+      "A carbon regeneration kiln being built by Caesar Furnace for Asante Gold - Mensin.",
+  },
+  {
+    image: "/images/cruci.jpg",
+    title: "CRUCIBLES",
+
+    description:
+      "Durable, high-performance containers designed for melting and holding metals at high temperatures in industrial and laboratory settings.",
+  },
+
+  {
+    image: "/images/burner.png",
+    title: "DIESEL AND GAS BURNERS",
+
+    description:
+      "Efficient diesel and gas burners engineered to deliver precise heat control and optimized fuel consumption for various industrial applications.",
+  },
+];
